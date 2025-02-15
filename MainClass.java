3120ab583e82d48abddfb8e6b8fc918c260615c3
Gourav <gourav1.velociter@gmail.com>
2025-02-15 10:16:20 +0530

    first Commit

---
+++ MainClass.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("Jenkins basic program");
+		System.out.println(" Date :"+new Date());
+
+	}
+
+}
